aaa5037aceeba0df0c37be3a2f3ff0e248ba8871
lenghialenghia <56626129+lenghialenghia@users.noreply.github.com>
2022-09-20 23:06:17 +0700

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,14 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 public class Solution {
@@ -27,7 +38,12 @@ public class Solution {
     }
 
 
+
+
     public static void main(String []args) {
+
+
+
         ArrayList<performance> performances = new ArrayList<performance>();
         performances.add(new performance("hamlet", 55));
         performances.add(new performance("as-like", 35));
@@ -35,14 +51,56 @@ public class Solution {
         Invoices invoice = new Invoices ("BigCo", performances);
 
         HashMap<String, Player> plays = new HashMap<String, Player>();
+
+        Object obj;
+        try {
+            obj = new JSONParser().parse(new FileReader("C:\\Users\\nghia\\Desktop\\btvn_w3-master\\btvn_w3-master\\src\\plays.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // đọc hamlet
+            String a; int i=1;
+            Map hamlet = ((Map) jsonObject.get("hamlet"));
+            // đọc hamlet Map
+            Iterator<Map.Entry> itr1 = hamlet.entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+
+            // đọc aslike
+            Map Aslike = ((Map) jsonObject.get("as-like"));
+            // đọc hamlet Map
+            Iterator<Map.Entry> itr2 = Aslike.entrySet().iterator();
+            while (itr2.hasNext()) {
+                Map.Entry pair = itr2.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+
+            // đọc othello
+            Map othello = ((Map) jsonObject.get("othello"));
+            // đọc hamlet Map
+            Iterator<Map.Entry> itr3 = othello.entrySet().iterator();
+            while (itr3.hasNext()) {
+                Map.Entry pair = itr3.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
         plays.put("hamlet", new Player("Hamlet", "tragedy"));
         plays.put("as-like", new Player("As You Like It", "comedy"));
         plays.put("othello", new Player("Othello", "tragedy"));
         try {
-            System.out.println(statement(invoice, plays));
+            //System.out.println(statement(invoice, plays));
+
         } catch (Exception e) {
             e.printStackTrace();
-
         }
     }
 }
